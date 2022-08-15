28dddcbb9ec5400f3d10eee36e67611083ae11c9
Lucas Chagas Magno <109705062+lucascmagno@users.noreply.github.com>
2022-08-15 17:41:52 -0300

    atualization

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,13 @@
 var sim = document.getElementById('sim')
+var nao = document.getElementById('nao')
+var div = document.getElementById('btn_nao')
 function clicou_sim(){
-    alert('ola')
+    alert('Nice bro 🤙')
+}
+function clicou_nao(){
+    var a = getRandom()
+    alert('Compreensível tenha um bom dia')
+}
+function getRandom(){
+    return Math.random() * 10;
 }
